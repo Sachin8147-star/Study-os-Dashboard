ad3535bfc9b639cd6d4f50fd726976e229a214cd
Sachin8147-star <Ks6911843@gmail.com>
2026-03-05 11:38:47 +0530

    prepare for deployment

--- studyos/settings.py
+++ studyos/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-studyos-change-this-in-production-xyz-123'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
